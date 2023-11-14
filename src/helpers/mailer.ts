f6f0b61318ddc0fb106de6e87e69d39166ada92a
Sai-Kokate <saikokatelearning@gmail.com>
2023-11-14 19:18:11 +0530

    fix mail domain

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -33,8 +33,8 @@ export const sendEmail = async({email, emailType, userId}:any) => {
             from: 'sai@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-            html: `<p>Click <a href="${process.env.DOMAIN}/${path}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
-            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/${path}?token=${hashedToken}
+            html: `<p>Click <a href="${process.env.DOMAIN!}/${path}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN!}/${path}?token=${hashedToken}
             </p>`
         }
 
